a5335bea925e9bac06d4f412c1618f1fb68acdf5
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-12-02 23:47:04 +0000

    Rock Paper Scissors — Part 2

--- 02-rock-paper-scissors.rb
+++ 02-rock-paper-scissors.rb
@@ -35,19 +35,44 @@ data = File.readlines(get_data_file(__FILE__), chomp: true)
     end
     return result + user
   },
+  part_2: ->(guide) {
+    opponent = guide.first
+    expected = guide.last
+
+    raise "Unknown instruction" unless %w( X Y Z ).include?(expected)
+
+    case
+    when to_win?(expected)
+      user_play = weigh(opponent) + 1
+      user_play = user_play % 3 if user_play > 3
+      result = @scores[:win]
+    when to_draw?(expected)
+      user_play = weigh(opponent)
+      result = @scores[:draw]
+    when to_lose?(expected)
+      user_play = weigh(opponent) - 1
+      user_play = 3 + user_play if user_play < 1
+      result = @scores[:lose]
+    end
+
+    return result + user_play
+  }
 }
 
 def is_rock? played
   %w(A X).include? played
 end
+alias :to_lose? :is_rock?
 
 def is_paper? played
   %w(B Y).include? played
 end
+alias :to_draw? :is_paper?
 
 def is_scissors? played
   %w(C Z).include? played
 end
+alias :to_win? :is_scissors?
 
 def weigh played
   case
@@ -66,7 +91,7 @@ def play hands, strategy: @strategy[:part_1]
   strategy.call(hands)
 end
 
-total = data.map { |e| play(e.split(' ')) }
+total = data.map { |e| play(e.split(' '), strategy: @strategy[:part_2]) }
 
 puts total.sum
 
