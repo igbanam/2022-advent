96f02d69a2bd7923b656b78ec8de8592f20eab89
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-12-04 22:30:17 +0000

    Camp Cleanup —·Part 2 (Verbose)
    If this is meant to be literate, might as well write everything out.

--- 04-camp-cleanup.rb
+++ 04-camp-cleanup.rb
@@ -7,8 +7,8 @@ data = File.readlines(get_data_file(__FILE__), chomp: true)
 overlaps = data.map do |line|
   !line.split(',').
     map { |range| range.split('-').map(&:to_i) }.
-    map { |d| (d[0]..d[1]).to_a }.
-    reduce(&:&).empty?
+    map { |range| (range.first..range.last).to_a }.
+    reduce(&:intersection).empty?
 end
 
 puts overlaps.count { |f| f == true }
