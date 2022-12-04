905c04fc635a28b0280ef703f9674e355887806b
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-12-04 22:23:44 +0000

    Camp Cleanup — Part 2
    For this one, I know it's highly inefficient, and should not be used for
    large arrays. I settled for this because it seemed the ranges were
    between [0, 100], and the number of test cases was 1000. Anything more
    than 10000 test cases with ranges possibly on the order of thousands may
    begin to crash this memory-wise.
    
    A more efficient way would be to check the bounds. It's faster, it
    doesn't require more memory space, it's leaner. However, I chose to
    trade that off for literacy. The idea is that someone who isn't too
    technical should be able to read this and understand what's going on.
    
    🤞🏾

--- 04-camp-cleanup.rb
+++ 04-camp-cleanup.rb
@@ -2,30 +2,15 @@ require './helpers.rb'
 
 include Helpers
 
-class ElfRange
-  attr_accessor :from, :to
-  def fully_contains? elf_range
-    from <= elf_range.from && to >= elf_range.to
-  end
+data = File.readlines(get_data_file(__FILE__), chomp: true)
+
+overlaps = data.map do |line|
+  !line.split(',').
+    map { |range| range.split('-').map(&:to_i) }.
+    map { |d| (d[0]..d[1]).to_a }.
+    reduce(&:&).empty?
 end
 
-data = File.readlines(get_data_file(__FILE__))
-containers = 0
-
-data.each do |line|
-  ranges = line.split(',').map do |range|
-    bounds = range.split('-').map(&:to_i)
-    elf_range = ElfRange.new
-    elf_range.from = bounds.first
-    elf_range.to = bounds.last
-    elf_range
-  end
-
-  if ranges.first.fully_contains?(ranges.last) || ranges.last.fully_contains?(ranges.first)
-    containers += 1
-  end
-end
-
-puts containers
+puts overlaps.count { |f| f == true }
 
 gets
