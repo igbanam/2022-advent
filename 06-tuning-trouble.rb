fe24ec98069a18443484c5857ab64b30fdd0bb20
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-12-06 12:59:01 +0000

    Tuning Trouble —·Part 2

--- 06-tuning-trouble.rb
+++ 06-tuning-trouble.rb
@@ -5,7 +5,7 @@ include Helpers
 data = File.readlines(get_data_file(__FILE__), chomp: true)
 
 signal = data.first
-WINDOW = 4
+WINDOW = 14
 
 signal.chars.each_cons(WINDOW).with_index do |window, index|
   if window.uniq.size == WINDOW
